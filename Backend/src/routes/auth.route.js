038df40d65354bd709d278252795f3bbfac6218a
Mandeep Malakar <tagdacoder@gmail.com>
2026-05-04 21:41:33 +0530

    fix: auth route file

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authMiddleware from "../middlewares/auth.middleware.js";
-import { getMe, login, logout, signup } from "../controllers/auth.controller.js";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -18,12 +18,6 @@ router.post("/login", login);
 // @desc     Logout User
 // @access   Private
 router.post("/logout", authMiddleware, logout);
-
-// @route    GET /api/auth/get-me
-// @desc     Get current logged in user
-// @access   Private
-router.get("/get-me", authMiddleware, getMe);
-
 
 // Export the router to be used in app.js
 export default router;
